db1648db20af42dab714f9347b99d345230173ef
Euphoria Yan <ysq58000@foxmail.com>
2020-01-15 23:22:49 +0800

    Update codes.

---
+++ create_pretraining_data_ciqushi.sh
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+input_file=corpus/classified_corpus/zh/ci_qu_shi.txt
+output_file=output/ci_qu_shi.tfrecord1
+vocab_file=vocab2.txt
+
+
+python create_pretraining_data.py \
+--input_file ${input_file} \
+--output_file ${output_file} \
+--vocab_file ${vocab_file} \
+--do_whole_word_mask True
