5ff93341b5c3cfedb5dc58d2676cdd7b64db2d93
Euphoria Yan <ysq58000@foxmail.com>
2020-01-20 13:45:37 +0800

    Update codes.

--- create_pretraining_data_ruzang.sh
+++ create_pretraining_data_ruzang.sh
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 input_file=corpus/classified_corpus/daizhige/儒藏_combine_ft.txt
-output_file=output/ruzang_data.tfrecord
+output_file=output/ruzang_data.tfrecord1
 vocab_file=vocab.txt
 
 
--- run_pretraining.sh
+++ run_pretraining.sh
@@ -5,7 +5,7 @@ output/ci_qu_shi.tfrecord1,\
 output/core.tfrecord1,\
 output/jizang_data.tfrecord1,output/jizang_data.tfrecord2,output/jizang_data.tfrecord3,\
 output/leishu.tfrecord1,\
-output/ruzang_data.tfrecord,\
+output/ruzang_data.tfrecord1,\
 output/shi1zang_data.tfrecord1,output/shi1zang_data.tfrecord2,\
 output/shi3zang_data.tfrecord1,\output/shi3zang_data.tfrecord2,output/shi3zang_data.tfrecord3,\
 output/zizang_data.tfrecord1,\
